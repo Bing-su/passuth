58eb8d9cf0279ff2369107c37319bce7276dfc75
Dowon <ks2515@naver.com>
2025-06-25 13:12:38 +0900

    test: add jsonpickle test, fixes

--- tests/fernet_test.py
+++ tests/fernet_test.py
@@ -1,15 +1,18 @@
 import copy
 import pickle
 
+import jsonpickle
 from hypothesis import given
 from hypothesis import strategies as st
 from passuth import Fernet
+
+TXT = "Hello, World!"
 
 
 @given(text=st.text(max_size=1_000_000))
 def test_fernet_encrypt_decrypt(text: str):
     fernet = Fernet(Fernet.generate_key())
-    encrypted = fernet.encrypt(text.encode())
+    encrypted = fernet.encrypt(text)
     decrypted = fernet.decrypt(encrypted).decode()
 
     assert decrypted == text
@@ -26,34 +29,46 @@ def test_fernet_encrypt_decrypt_bytes(binary: bytes):
 
 def test_fernet_pickle():
     fernet = Fernet(Fernet.generate_key())
-    encrypted = fernet.encrypt("Hello, World!")
+    encrypted = fernet.encrypt(TXT)
 
     pickled_fernet = pickle.dumps(fernet)
     unpickled_fernet = pickle.loads(pickled_fernet)  # noqa: S301
 
     decrypted = unpickled_fernet.decrypt(encrypted).decode()
 
-    assert decrypted == "Hello, World!"
+    assert decrypted == TXT
+
+
+def test_fernet_jsonpickle():
+    fernet = Fernet.new()
+    encrypted = fernet.encrypt(TXT)
+
+    pickled_fernet = jsonpickle.dumps(fernet)
+    unpickled_fernet: Fernet = jsonpickle.loads(pickled_fernet)  # pyright: ignore[reportAssignmentType]
+
+    decrypted = unpickled_fernet.decrypt(encrypted).decode()
+
+    assert decrypted == TXT
 
 
 def test_fernet_copy():
     fernet = Fernet.new()
-    encrypted = fernet.encrypt("Hello, World!")
+    encrypted = fernet.encrypt(TXT)
 
     copied_fernet = copy.copy(fernet)
     decrypted = copied_fernet.decrypt(encrypted).decode()
 
-    assert decrypted == "Hello, World!"
+    assert decrypted == TXT
 
 
 def test_fernet_deepcopy():
     fernet = Fernet(Fernet.generate_key())
-    encrypted = fernet.encrypt("Hello, World!")
+    encrypted = fernet.encrypt(TXT)
 
     deepcopied_fernet = copy.deepcopy(fernet)
     decrypted = deepcopied_fernet.decrypt(encrypted).decode()
 
-    assert decrypted == "Hello, World!"
+    assert decrypted == TXT
 
 
 def test_fernet_repr():
