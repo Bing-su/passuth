fef50ddd2ff0ee3fec9efb68b7a6aec792db5322
Dowon <ks2515@naver.com>
2025-08-07 22:09:22 +0900

    ci: fix nox python installation

--- noxfile.py
+++ noxfile.py
@@ -1,13 +1,10 @@
-import sysconfig
-
 import nox
 
 
 @nox.session(python=["3.9", "3.13", "3.13t", "pypy3.11"], venv_backend="uv")
 def test(session: nox.Session) -> None:
-    py_gil_disabled = sysconfig.get_config_var("Py_GIL_DISABLED")
-    if py_gil_disabled:
-        session.install(".[test]")
-    else:
+    if isinstance(session.python, str) and "t" not in session.python:
         session.install(".[test,test-extra]")
+    else:
+        session.install(".[test]")
     session.run("pytest", "-v", *session.posargs)
