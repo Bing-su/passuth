015a003349bde8b690bd0f073e944952c700c12a
Dowon <ks2515@naver.com>
2025-10-14 16:25:55 +0900

    chore(ci): update python test matrix

--- noxfile.py
+++ noxfile.py
@@ -1,9 +1,12 @@
+import re
+
 import nox
 
 
-@nox.session(python=["3.9", "3.13", "3.13t", "pypy3.11"], venv_backend="uv")
+
+@nox.session(python=["3.9", "3.14", "3.14t", "pypy3.11", "graalpy3.12"], venv_backend="uv")
 def test(session: nox.Session) -> None:
-    if isinstance(session.python, str) and "t" not in session.python:
+    if isinstance(session.python, str) and re.match(r"^3\.\d+$", session.python):
         session.install(".[test,test-extra]")
     else:
         session.install(".[test]")
