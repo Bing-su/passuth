7ab7f7e7970568ecbc2d9da0b3f64efcbbd5b777
Dowon <ks2515@naver.com>
2025-10-23 14:58:00 +0900

    chore(deps): Update pyo3 and configure GraalPy

--- noxfile.py
+++ noxfile.py
@@ -1,13 +1,16 @@
 import re
+import platform
 
 import nox
 
+py = ["3.9", "3.14", "3.14t", "pypy3.11"]
+if platform.system() != "Windows":
+    py.append("graalpy3.12")
 
-@nox.session(
-    python=["3.9", "3.14", "3.14t", "pypy3.11", "graalpy3.12"], venv_backend="uv"
-)
+
+@nox.session(python=py, venv_backend="uv")
 def test(session: nox.Session) -> None:
-    if isinstance(session.python, str) and re.match(r"^3\.\d+$", session.python):
+    if isinstance(session.python, str) and re.match(r"^3\.\d+t?$", session.python):
         session.install(".[test,test-extra]")
     else:
         session.install(".[test]")
